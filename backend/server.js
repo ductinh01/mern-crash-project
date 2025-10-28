f52e278f0c2d734e6d2196ecc965003fa125eaf3
Đại Ca Tính <macos@192.168.2.8>
2025-10-28 19:53:13 +0700

    Chakra UI setup

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,13 @@ dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log("Server started at http://localhost:" + PORT);
 });
